0cc323e4902ad3806ba36a53eedcfe0631f92fd3
HassanObeid89 <61018141+HassanObeid89@users.noreply.github.com>
2020-11-04 21:40:52 +0100

    Structure the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import logo from './assets/logo.svg';
-
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      
     </div>
   );
 }
